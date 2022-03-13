c2b865fc5c176788aeb41cd2e2d9fc806e772206
Adam Smith <91632531+Roadlyfe@users.noreply.github.com>
2022-03-13 14:23:41 -0500

    finished header

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,5 @@
 
-
-const Header = (title, date, temp) => {
-  const header = document.createElement('div'); 
-  const headerTitle = document.createElement('h1');
-  const headerDate = document.createElement('span');
-  const headerTemp = document.createElement('span');
-
-  // headerDate.classList.add('card'); 
-  headerDate.classList.add('date'); 
-  headerTitle.classList.add('title');
-  headerTemp.classList.add('temp'); 
- 
-  
-  headerDate.textContent = "March 11th, 2022";
-  headerTitle.textContent = "Lambda Times"; 
-  headerTemp.textContent = "23º";
-
-  const insertHeader = document.querySelector('header-container'); 
-  // header.appendChild(date);
-  // header.appendChild(title);
-  // header.appendChild(temp);
-
-
-  // const  = document.querySelector('header');
-return header;
-  // TASK 1
+// TASK 1
   // ---------------------
   // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
   // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
@@ -36,16 +11,52 @@ return header;
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+
+  // am I supposed to be creating an object here?? 
+
+
+  // const headerContainer = document.querySelector(".header-container");
+
+  const Header = (title, date, temp) => {
+    const header = document.createElement("div");
+    const span1 = document.createElement("span");
+    const h1 = document.createElement("h1");
+    const span2 = document.createElement("span");
+
+    header.classList.add("header");
+    span1.textContent = date;
+    h1.textContent = title;
+    span2.textContent = temp;
+
+    span1.classList.add("date");
+    span2.classList.add("temp");
+
+    header.appendChild(span1);
+    header.appendChild(h1);
+    header.appendChild(span2);
+
+
+    
+console.log(header)
+    return header;
 }
 
-const headerAppender = (selector) => {
-  const entryPoint = document.querySelector(selector);
-  // TASK 2
+// TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+
+
+const headerAppender = (selector) => {
+  const header = document.querySelector(selector);
+  const txt = "Lambda School";
+  const testvar = Header(txt, "January 6th, 2021", "temp")
+  header.appendChild(testvar);
+  return header;
 }
+// headerAppender();
+
 
 export { Header, headerAppender }
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,5 +1,4 @@
-const Tabs = (topics) => {
-  // TASK 3
+// TASK 3
   // ---------------------
   // Implement this function which takes an array of strings ("topics") as its only argument.
   // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
@@ -13,16 +12,47 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
+
+const Tabs = (topics) => {
+  const tab1 = document.createElement('div');
+  const tab2 = document.createElement('div');
+  const tab3 = document.createElement('div');
+  const tab4 = document.createElement('div');
+  const tab5 = document.createElement('div');
+
+  
+  tab1.appendChild(tab2);
+  tab2.appendChild(tab3);
+  tab3.appendChild(tab4);
+  tab4.appendChild(tab5);
+  
 }
 
-const tabsAppender = (selector) => {
-  // TASK 4
+ // TASK 4
   // ---------------------
   // Implement this function which takes a css selector as its only argument.
   // It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it with a console.log!).
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
-}
 
+  
+const tabsAppender = (selector) => {
+  // const entryPoint = document.querySelector(selector);
+  // axios.get("http://localhost:5000/api/topics")
+  // .then(res => {
+  //   console.log(res.data.message);
+  //   res.data.message.forEach(image => {
+  //     const tabs = tabsAppender({ topics });
+  //     entryPoint.appendChild(tabs);
+  //   })
+  // })
+  // .catch(err => {
+  //   console.error(err);
+  // })
+  // .finally(() => {
+  //   console.log("IT'S WORKING, IT'S WORKING!");
+  // })
+}
+ 
 export { Tabs, tabsAppender }
